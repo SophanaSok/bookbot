1764310cba1e1a475f48c3df47ac83433b7f4d67
SophanaSok <sokdevelopment@gmail.com>
2024-05-29 20:57:00 -0700

    update main.py to sort and print report

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     file_contents = read_book(book_path)
     words = count_words(file_contents)
+    list_of_dict = sort_dict(char_counter_dict(file_contents))
+    list_of_dict.sort(reverse=True, key=sort_on)
     
+    print("---------- REPORT -----------")
     print(f"{len(words)} words found in the document")
-    print_alpha_count(char_counter_dict(file_contents))
-    print(char_counter_dict(file_contents))
+    print_alpha_count(list_of_dict)
+    print("---------- END OF REPORT -----------")
 
 def read_book(book):
     with open(book) as f:
@@ -26,11 +29,20 @@ def char_counter_dict(words):
                 char_list[char] += 1
     return char_list
 
-def print_alpha_count(dict):
+def print_alpha_count(list):
+    for i in list:
+        print(f"The {i['name']} character was found {i['num']} times")
+            #print(f"The '{i}' character was found {dict[i]} times")
+
+def sort_dict(dict):
+    sort_list = []
     for i in dict:
         if i.isalpha() == True:
-            print(f"The '{i}' character was found {dict[i]} times")
+            sort_list.append({"name": i, "num": dict[i]})
+    return sort_list
 
+def sort_on(dict):
+    return dict["num"]
 
 
 main()
